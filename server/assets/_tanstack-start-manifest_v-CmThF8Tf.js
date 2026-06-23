9ff597e6b04747524205c816a5e1b54a0c59ee2d
vishwakarmakambar-pavero <vishwakarma.kambar@pavero.co>
2026-06-23 14:01:17 +0000

    Updates

--- server/assets/_tanstack-start-manifest_v-CmThF8Tf.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/workspaces/calendly-meeting/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DGSeLoPG.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DGSeLoPG.js" } }] }, "/": { filePath: "/workspaces/calendly-meeting/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-zuTWftcD.js"] } } });
-export {
-  tsrStartManifest
-};
